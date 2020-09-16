e90300134a9ca0167236cdc45b4eccf1f5920903
honshin4n1 <million_films.sh@icloud.com>
2020-09-16 19:18:04 +0900

    Update PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -49,7 +49,8 @@ class PostController extends Controller
      */
     public function show($id)
     {
-
+        $post = Post::findOrFail($id);
+        return view('posts.show', compact('post'));
     }
 
     /**
