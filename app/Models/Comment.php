2760ec8d17166d6148f22cb22d1a43657c57dfa8
honshin4n1 <million_films.sh@icloud.com>
2020-09-18 14:29:08 +0900

    Update Comment.php

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -9,4 +9,9 @@ class Comment extends Model
     //
 
     protected $fillable = ['replayer', 'body', 'post_id'];
+
+    public function post()
+    {
+        return $this->belongsTo('App\Model\Post');
+    }
 }
